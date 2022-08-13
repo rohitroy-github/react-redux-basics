6b13e53de758bc7841444e2ef13bda3743728129
rohit <rohitdevops.roy@gmail.com>
2022-08-14 02:45:33 +0530

    Update ...

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,50 +1,91 @@
-import React from 'react'; 
+import React from "react";
 
-import { useDispatch } from 'react-redux/es/exports';
+import { useDispatch } from "react-redux/es/exports";
 
 //For binding actionsCreators with dispath
-import { bindActionCreators } from 'redux';
+import { bindActionCreators } from "redux";
 
 //Importing actionCreators where reducers are created
-import { actionCreators } from '../state/index'; 
+import { actionCreators } from "../state/index";
 
-//Used for reading the state.values 
+//Used for reading the state.values
 import { useSelector } from "react-redux";
 
-import './Shop.css'; 
+import "./Shop.css";
 
 const Shop = () => {
-
-  //useDispatch > a Hook used to alter state 
+  //useDispatch > a Hook used to alter state
   const dispatch = useDispatch();
 
-  const { withdrawMoney, depositMoney } = bindActionCreators(actionCreators, dispatch);
+  const { withdrawMoney, depositMoney } = bindActionCreators(
+    actionCreators,
+    dispatch
+  );
 
-  // Displaying the uodated balance 
-  const balance = useSelector (state  => state.amount);
+  // Displaying the uodated balance
+  const balance = useSelector((state) => state.amount);
+
+  if(balance < 0){ 
+    alert('Your account balance is 0 | Please deposit more money !');
+  }
 
   return (
-    <div className='main-container container'> 
-      <div className='heading'> 
+    <div className="main-container container">
+      <div className="heading">
         <h3>React-Redux Application</h3>
-        <h4 className='small-heading'>Basic Bank Account Balance Updation Application Using Redux Store</h4>
+        <h4 className="small-heading">
+          Basic Bank Account Balance Updation Application Using Redux Store
+        </h4>
       </div>
-      <div className='update-container'> 
-      {/* <h3>Calculation Checks</h3> */}
-        <button type="button" className="button-inc btn btn-primary" onClick={() => { 
-          depositMoney(100)
-        }}><h4>Deposit Money</h4></button>
-        
-        <button type="button" className="button-dec btn btn-primary" onClick={() => { 
-          withdrawMoney(100)
-        }}><h4>Withdraw Money</h4></button>
+
+      <div className="update-container">
+        {/* <h3>Calculation Checks</h3> */}
+        <button
+          type="button"
+          className="button-inc btn btn-primary"
+          onClick={() => {
+            depositMoney(100);
+          }}
+        >
+          <h4>Deposit Money</h4>
+        </button>
+
+        <button
+          type="button"
+          className="button-dec btn btn-primary"
+
+          onClick={() => {
+            if(balance < 0){ 
+              alert('No more withdrawing possible, account balance is 0 !');
+            }
+            else{ 
+              withdrawMoney(100);
+            }
+          }}
+        >
+          <h4>Withdraw Money</h4>
+        </button>
+
+        {/* <div class="amount-field input-group input-group-lg">
+          <input
+            type="number"
+            class="form-control"
+            placeholder="Enter Amount"
+            aria-label="Enter Amount"
+            aria-describedby="basic-addon1"
+            value={amountToUpdate}
+          />
+        </div> */}
       </div>
-      <div className='balance-container'> 
+
+      <div className="balance-container">
         <h4>Updated Account Balance</h4>
-        <button type="button" className="button-updt btn btn-dark active"><h4>{balance}</h4></button>
+        <button type="button" className="button-updt btn btn-dark active">
+          <h4>{balance}</h4>
+        </button>
       </div>
     </div>
-  )
+  );
 };
 
 export default Shop;
